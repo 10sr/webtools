0cd9d6ebbf4c420b3c2f803befc0fa66b108ce05
10sr <8.slashes@gmail.com>
2019-05-28 14:24:34 +0900

    Remove  omment

--- export_as_bookmark/redis.py
+++ export_as_bookmark/redis.py
@@ -2,15 +2,6 @@ from urllib.parse import urlparse
 
 import redis
 
-# def _parse_url(url):
-#     parsed = urlparse(url)
-#     assert parsed.scheme == "redis"
-#     host_port = parsed.netloc.split(":")
-#     assert len(host_port) == 2
-#     host, port = host_port
-#     database = parsed.ltrim("/")
-#     return (host, port, database)
-
 
 class Redis:
     __client = None
@@ -19,11 +10,9 @@ class Redis:
         self.url = settings.EXPORT_AS_BOOKMARK_REDIS_URL
         return
 
-
     @property
     def _client(self):
         if self.__client is None:
-            # parsed = _parse_url(self.url)
             # TODO: How to reuse redis client???
             self.__client = redis.Redis.from_url(self.url)
         return self.__client
