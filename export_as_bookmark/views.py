c2dc62c307fbc6f5cee89b2cd53efa11f0202ae5
10sr <8.slashes@gmail.com>
2019-06-07 00:04:59 +0900

    Kill referer

--- export_as_bookmark/views.py
+++ export_as_bookmark/views.py
@@ -42,10 +42,11 @@ def done(req: HttpRequest, id: str, name: str) -> HttpResponse:
 
 
 def download(req: HttpRequest, id: str, name: str) -> HttpResponse:
-    # TODO: Kill referer
-    # TODO: Ur use req.session?
+    # TODO: Use req.session?
     # https://docs.djangoproject.com/en/2.1/ref/request-response/#django.http.HttpRequest.session
     val = Redis.get(id)
     if val is None:
         return HttpResponseNotFound(f"Content of id {id[:24]} not found. Expired?")
-    return HttpResponse(val.decode("utf-8"))
+    res = HttpResponse(val.decode("utf-8"))
+    res["Referrer-Policy"] = "origin"
+    return res
