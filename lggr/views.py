d52b562b7045190a72f03d5c6ba7891beabbba82
10sr <8.slashes@gmail.com>
2019-06-06 13:45:57 +0900

    Output log to client

--- lggr/views.py
+++ lggr/views.py
@@ -15,24 +15,42 @@ logger = logging.getLogger(__name__)
 
 def get(req: HttpRequest) -> HttpResponse:
     request_id = id(req)
-    logger.warning(f"Id: {request_id} get/ Requested >>>>>")
-    logger.warning(f"Id: {request_id} req.META:")
-    logger.warning(f"Id: {request_id} {pprint.pformat(req.META)}")
-    logger.warning(f"Id: {request_id} req.GET:")
-    logger.warning(f"Id: {request_id} {pprint.pformat(req.GET.dict())}")
-    logger.warning(f"Id: {request_id} get/ Requested <<<<<")
-    return HttpResponse("")
+    log = f"""Id: {request_id} get/ Requested >>>>>
+req.META:
+{pprint.pformat(req.META)}
+req.GET:
+{pprint.pformat(req.GET.dict())}
+Id: {request_id} get/ Requested <<<<<
+"""
+    # logger.warning(f"Id: {request_id} get/ Requested >>>>>")
+    # logger.warning(f"Id: {request_id} req.META:")
+    # logger.warning(f"Id: {request_id} {pprint.pformat(req.META)}")
+    # logger.warning(f"Id: {request_id} req.GET:")
+    # logger.warning(f"Id: {request_id} {pprint.pformat(req.GET.dict())}")
+    # logger.warning(f"Id: {request_id} get/ Requested <<<<<")
+    logger.warning(log)
+    return HttpResponse(log, content_type="text/plain")
 
 # curl -X POST -d @a.txt localhost:7700/webtools/lggr/post
 @csrf_exempt
 def post(req: HttpRequest) -> HttpResponse:
     request_id = id(req)
-    logger.warning(f"Id: {request_id} post/ Requested >>>>>")
-    logger.warning(f"Id: {request_id} req.META:")
-    logger.warning(f"Id: {request_id} {pprint.pformat(req.META)}")
-    logger.warning(f"Id: {request_id} req.body:")
-    logger.warning(f"Id: {request_id} {repr(req.body)}")
-    logger.warning(f"Id: {request_id} req.POST:")
-    logger.warning(f"Id: {request_id} {pprint.pformat(req.POST.dict())}")
-    logger.warning(f"Id: {request_id} post/ Requested <<<<<")
-    return HttpResponse("")
+    log = f"""Id: {request_id} post/ Requested >>>>>
+req.META:
+{pprint.pformat(req.META)}
+req.body:
+{repr(req.body)}
+req.POST:
+{pprint.pformat(req.POST.dict())}
+Id: {request_id} post/ Requested <<<<<
+"""
+    # logger.warning(f"Id: {request_id} post/ Requested >>>>>")
+    # logger.warning(f"Id: {request_id} req.META:")
+    # logger.warning(f"Id: {request_id} {pprint.pformat(req.META)}")
+    # logger.warning(f"Id: {request_id} req.body:")
+    # logger.warning(f"Id: {request_id} {repr(req.body)}")
+    # logger.warning(f"Id: {request_id} req.POST:")
+    # logger.warning(f"Id: {request_id} {pprint.pformat(req.POST.dict())}")
+    # logger.warning(f"Id: {request_id} post/ Requested <<<<<")
+    logger.warning(log)
+    return HttpResponse(log, content_type="text/plain")
