aa42fe3f3f554be12904be0fe6d4e8798b4f84c9
10sr <8.slashes@gmail.com>
2019-06-10 13:19:13 +0900

    Make settings_module config constant

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webtools.settings")
+    os.environ["DJANGO_SETTINGS_MODULE"] = "webtools.settings"
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
