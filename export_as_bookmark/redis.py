8398e31bd07568de8a3b1b6e1e39fd3279db5f94
10sr <8.slashes@gmail.com>
2019-06-24 15:56:52 +0900

    Revert "Fix some errors reported by flake8-mypy"
    This reverts commit 5a69919041a96e41fae78fbaf7c1711d4b131f69.

--- export_as_bookmark/redis.py
+++ export_as_bookmark/redis.py
@@ -49,15 +49,12 @@ class Redis:
         :returns: Redis client instance
         """
         if cls.__client is None:
-            cls.__client = cast(
-                # Call to untyped function "from_url" of "Redis" in typed context
-                redis.Redis,
-                redis.Redis.from_url(cls.url),  # type: ignore
-            )
+            cls.__client = cast(redis.Redis, redis.Redis.from_url(cls.url))
         return cls.__client
 
     @classmethod
-    def set(cls, k: str, v: bytes, **kargs: Any) -> Any:
+    # Should use Any for kargs?
+    def set(cls, k: str, v: bytes, **kargs: Union[int, str]) -> Any:
         """
         Set key-value pair.
 
