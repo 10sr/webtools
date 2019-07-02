e11f1a54f8b24868a85c7ca48b220884388b9d16
10sr <8.slashes@gmail.com>
2019-07-02 13:49:02 +0900

    Parametrize static dir in script

--- download_semanticui.sh
+++ download_semanticui.sh
@@ -5,12 +5,13 @@ set -o pipefail
 SEMANTIC_UI_VERSION=2.4.1
 
 tarball=Semantic-UI-CSS-$SEMANTIC_UI_VERSION.tar.gz
+static_dir=static
 
 # https://github.com/Semantic-Org/Semantic-UI-CSS
 wget https://github.com/Semantic-Org/Semantic-UI-CSS/archive/$SEMANTIC_UI_VERSION.tar.gz \
      -O $tarball
 
 tar -vxf $tarball
-rm -rfv static/semanticui/
-mkdir -p static
-cp -rv Semantic-UI-CSS-$SEMANTIC_UI_VERSION/ static/semanticui/
+rm -rfv $static_dir/semanticui/
+mkdir -p $static_dir
+cp -rv Semantic-UI-CSS-$SEMANTIC_UI_VERSION/ $static_dir/semanticui/
