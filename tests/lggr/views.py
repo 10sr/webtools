e59db076b59b0124b4e3c879b7d6c5adbf317046
10sr <8.slashes@gmail.com>
2019-07-08 13:34:30 +0900

    Fix method name

--- tests/lggr/views.py
+++ tests/lggr/views.py
@@ -5,15 +5,15 @@ from django.urls import reverse
 
 
 class ViewTests(TestCase):
-    def _get_client(self, name, *args, **kargs):
+    def _request_get(self, name, *args, **kargs):
         return self.client.get(reverse(f"lggr:{name}"), *args, **kargs)
 
     def test_index(self):
-        response = self._get_client("index")
+        response = self._request_get("index")
         self.assertEqual(response.status_code, 200)
         return
 
     def test_get(self):
-        response = self._get_client("get", {"key1": "v1", "key2": "value2"})
+        response = self._request_get("get", {"key1": "v1", "key2": "value2"})
         self.assertEqual(response.status_code, 200)
         return
