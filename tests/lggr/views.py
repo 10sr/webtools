5876a5d45ea9aecdc3c8bd4d6430b67609a27432
10sr <8.slashes@gmail.com>
2019-07-09 17:45:21 +0900

    Add test_post

--- tests/lggr/views.py
+++ tests/lggr/views.py
@@ -8,6 +8,9 @@ class ViewTests(TestCase):
     def _request_get(self, name, *args, **kargs):
         return self.client.get(reverse(f"lggr:{name}"), *args, **kargs)
 
+    def _request_post(self, name, *args, **kargs):
+        return self.client.post(reverse(f"lggr:{name}"), *args, **kargs)
+
     def test_index(self):
         response = self._request_get("index")
         self.assertEqual(response.status_code, 200)
@@ -15,5 +18,10 @@ class ViewTests(TestCase):
 
     def test_get(self):
         response = self._request_get("get", {"key1": "v1", "key2": "value2"})
+        self.assertEqual(response.status_code, 200)
+        return
+
+    def test_post(self):
+        response = self._request_post("post", {"key1": "v1", "key2": "value2"})
         self.assertEqual(response.status_code, 200)
         return
