dd5fe6df40290765e1ff2f8d7cd37ed7fc44883a
10sr <8.slashes@gmail.com>
2019-07-09 18:07:05 +0900

    Add test_post, still failing

--- tests/export_as_bookmark/views.py
+++ tests/export_as_bookmark/views.py
@@ -6,7 +6,22 @@ from django.urls import reverse
 
 
 class ViewTests(TestCase):
+    def _request_get(self, name, *args, **kargs):
+        return self.client.get(reverse(f"export_as_bookmark:{name}"), *args, **kargs)
+
+    def _request_post(self, name, *args, **kargs):
+        return self.client.post(reverse(f"export_as_bookmark:{name}"), *args, **kargs)
+
     def test_index(self):
-        response = self.client.get(reverse("export_as_bookmark:index"))
+        response = self._request_get("index")
         self.assertEqual(response.status_code, 200)
+        return
+
+    def test_post(self):
+        body = """http://google.com
+http://yahoo.co.jp
+"""
+        # TODO: Mock redis client
+        response = self._request_post("post", {"body": body})
+        self.assertEqual(response.status_code, 302)
         return
