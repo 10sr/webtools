d41c6a804b90805671dc9fbc4ee668575d1ab772
10sr <8.slashes@gmail.com>
2019-09-19 19:50:21 +0900

    Fix format

--- webtools/config.py
+++ webtools/config.py
@@ -34,7 +34,7 @@ class Config:
     AWS_BUCKET_ACL: Optional[str] = "public-read"
     AWS_AUTO_CREATE_BUCKET: bool = True
     AWS_S3_ENDPOINT_URL: Optional[str] = None
-    #AWS_S3_CUSTOM_DOMAIN: Optional[str] = None
+    # AWS_S3_CUSTOM_DOMAIN: Optional[str] = None
     # Local Filesystem
     STATIC_ROOT: str = "static_deploy/webtools/static/"
 
