e953e4e4cda2ad831c689d27f08c496f909611d8
10sr <8.slashes@gmail.com>
2019-12-17 18:41:11 +0900

    Add redis mock

--- tests/export_as_bookmark/views.py
+++ tests/export_as_bookmark/views.py
@@ -1,11 +1,14 @@
-# import unittest
+"""View test."""
+
+from unittest import mock
 
 from django.test import TestCase
-
 from django.urls import reverse
+from export_as_bookmark.redis import Redis
 
 
 class ViewTests(TestCase):
+    """View test."""
     def _request_get(self, name, *args, **kargs):
         return self.client.get(reverse(f"export_as_bookmark:{name}"), *args, **kargs)
 
@@ -17,11 +20,12 @@ class ViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         return
 
-    def test_post(self):
+    @mock.patch.object(Redis, "get_instance")
+    def test_post(self, redis_mock):
         body = """http://google.com
 http://yahoo.co.jp
 """
-        # TODO: Mock redis client
         response = self._request_post("post", {"body": body})
         self.assertEqual(response.status_code, 302)
+        redis_mock.return_value.set.assert_called_once()
         return
